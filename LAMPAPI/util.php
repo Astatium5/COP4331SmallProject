26cb48380c610a73c46b49069a5f9555228bdec4
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-10 17:11:39 -0500

    trying to fix the login

--- LAMPAPI/util.php
+++ LAMPAPI/util.php
@@ -66,7 +66,8 @@
 		if (strlen($login) >= 20)
 			returnWithError("The login is invalid. length" . $login);
 		else if ($login === NULL)
-			returnWithError("The login is invalid. null" . $login);
+			returnWithError($login);
+			// returnWithError("The login is invalid. null" . $login);
 		else if (strlen($password) >= 70 || $password == NULL)
 			returnWithError("The password is invalid.");
 		else if (strlen($firstName) >= 30 || $firstName == NULL)
