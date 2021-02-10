b0eecebe15b7f6ed868676c78a0bc93b7d552011
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-10 02:33:22 -0500

    trying to fix register

--- js/app.js
+++ js/app.js
@@ -70,7 +70,7 @@ function doRegister() {
     const last = document.getElementById("registerLast").value;
 	const hash = md5(password);
 	
-	document.getElementById("registerContactResult").innerHTML = "";
+	document.getElementById("registerResult").innerHTML = "";
 
 	let obj = new Object();
 	obj.login = login;
