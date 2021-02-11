cbd93eac939fe4efe6d4f1e1794ca374fdb668de
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-11 00:14:06 -0500

    fixed all the fixable error of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'rules': {
-    'linebreak-style': ["error", "windows"],
-    'indent': [2, "tab"], 
-    'no-tabs': ["error", { allowIndentationTabs: true}]
-  },
+	'env': {
+		'browser': true,
+		'es2021': true,
+	},
+	'extends': [
+		'google',
+	],
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module',
+	},
+	'rules': {
+		'linebreak-style': ['error', 'windows'],
+		'indent': [2, 'tab'],
+		'no-tabs': ['error', {allowIndentationTabs: true}],
+	},
 };
