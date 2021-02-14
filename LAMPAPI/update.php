2a88d05f9941ae36caa3aa44f7fe8605eaf974c0
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-14 04:15:21 -0500

    added partial update to the update API

--- LAMPAPI/update.php
+++ LAMPAPI/update.php
@@ -5,7 +5,7 @@
 	
 	checkContact($inData["firstName"], $inData["lastName"], $inData["phone"],
 				 			 $inData["email"], $inData["address"], $inData["city"], 
-				 				$inData["state"], $inData["zip"]);
+				 			 $inData["state"], $inData["zip"]);
 				 
 	$uid = $inData["uid"];
 	$cid = $inData["cid"];
@@ -19,29 +19,75 @@
 	$zip = $inData["zip"];
 
 	$conn = db_connection();
-
 	if ($conn->connect_error)  {
 		returnWithErrorContact($conn->connect_error);
-	} 
-	else {
-		$sql = "UPDATE CONTACTS
-				SET firstName = '" . $firstName .
-				"', lastName = '" . $lastName . 
-				"', phone = '" . $phone .
-				"', email = '" . $email . 
-				"', address = '" . $address . 
-				"', city = '" . $city . 
-				"', state = '" . $state . 
-				"', zip = '" . $zip . "'" . 
-				"WHERE cid = " . $cid . ";";
+	} else {
+		$sql = "SELECT FROM CONTACTS WHERE cid=" . $cid . ";";
+		$result = $conn->query($sql);
 
-		if ($result = $conn->query($sql) != TRUE) {
-			returnWithErrorContact("No Records Found");
-		} else {
-			// actually returns no error
-			returnWithErrorContact("");
+		if ($result->num_rows > 0) {
+			$currentFirstName = $row["firstName"];
+			$currentLastName = $row["larstName"];
+			$currentPhone = $row["phone"];
+			$currentEmail = $row["email"];
+			$currentAddress = $row["address"];
+			$currentCity = $row["city"];
+			$currentState = $row["state"];
+			$currentZip = $row["zip"];
+
+			if ($currentFirstName != $firstName && $firstName == "") {
+				$firstName = $currentFirstName;
+			}
+
+			if ($currentLastName != $lastName && $lastName == "") {
+				$lastName = $currentLastName;
+			}
+
+			if ($currentPhone != $phone && $phone == "") {
+				$phone = $currentPhone;
+			}
+
+			if ($currentEmail != $email && $email == "") {
+				$email = $currentEmail;
+			}
+
+			if ($currentAddress != $address && $address == "") {
+				$address = $currentAddress;
+			}
+
+			if ($currentCity != $city && $city == "") {
+				$city = $currentCity;
+			}
+
+			if ($currentState != $state && $state == "") {
+				$state = $currentState;
+			}
+
+			if ($currentZip != $zip && $zip == "") {
+				$zip = $currentZip;
+			}
 		}
-		
-		$conn->close;
 	}
+
+	
+	$sql = "UPDATE CONTACTS
+					SET firstName = '" . $firstName .
+					"', lastName = '" . $lastName . 
+					"', phone = '" . $phone .
+					"', email = '" . $email . 
+					"', address = '" . $address . 
+					"', city = '" . $city . 
+					"', state = '" . $state . 
+					"', zip = '" . $zip . "'" . 
+					"WHERE cid = " . $cid . ";";
+
+	if ($result = $conn->query($sql) != TRUE) {
+		returnWithErrorContact("No Records Found");
+	} else {
+		// actually returns no error
+		returnWithInfoContact($uid, $cid, $firstName, $lastName, $phone, 
+													$email, $address, $city, $state, $zip);
+	}
+		
+	conn->close();
 ?>
