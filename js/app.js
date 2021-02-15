6bea2389d69f3fcd180fefc2e8b6e7cf5bdd864d
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-15 00:49:10 -0500

    adding uid data attribute to html

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ function doLogin() {
 
 				window.location.href = 'contacts.html';
 				let contactsUserName = document.getElementById('contactsUserName');
-				contactsUserName.innerHTML = 'Signed as ' + firstName + lastName;
+				contactsUserName.innerHTML = 'Signed in as: ' + firstName + lastName;
 				contactsUserName.dataset.indexNumber = userId;
 			}
 		};
