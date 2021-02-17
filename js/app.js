0b7a90281d86791c2c3fb798aab20b306b628d89
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-17 03:02:53 -0500

    fixing the manage button

--- js/app.js
+++ js/app.js
@@ -453,8 +453,7 @@ function addContactToTable(jsonObject) {
 // this function reads the data of the selected json and those become the elements in the update form
 function manageContact(id) {
 	const editor = document.getElementById('editor');
-	console.log(editor.getAttribute('hidden'));
-	editor.getAttribute('hidden') == 'true' ? editor.setAttribute('hidden', 'false') : editor.setAttribute('hidden', 'true');
+	editor.style.display == 'none' ? editor.style.display = 'block' : editor.style.diplay = 'none';
 
 	const row = document.getElementById(id).parentElement.parentElement;
 
