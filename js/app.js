38b5d7cdc25b4b11202cb12464215753cf2e0892
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-17 04:08:28 -0500

    fixing the update

--- js/app.js
+++ js/app.js
@@ -236,7 +236,7 @@ function updateContact() {
 	const jsonPayload = JSON.stringify(obj);
 	const url = urlBase + '/update' + extension;
 
-	console.log(uid + cid);
+	console.log(uid + ' ' + cid);
 	const xhr = new XMLHttpRequest();
 	xhr.open('POST', url, true);
 	xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
