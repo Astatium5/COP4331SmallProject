89c072af32bf724873df27537cdf45fd1093fdb5
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-02-17 17:25:29 -0500

    fixing the message after the create contact

--- js/app.js
+++ js/app.js
@@ -491,5 +491,5 @@ function toggleEdit() {
 
 function toggleAddContact() {
 	document.getElementById('createContactResult').innerHTML = '';
-	document.getElementById('closeAddContactModalButton').setAttribute('data-bs-dismiss', 'modal');
+	document.getElementById('closeAddContactModalButton').setAttribute('data-dismiss', 'modal');
 }
